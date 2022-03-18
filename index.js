240aa2c241c981e70c7e0c746c652671aeaa4a5f
MicaMore <micael@email.com>
2022-03-18 19:54:18 -0300

    aula 10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
@@ -8,8 +9,12 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/index", (req, res) => {
-    res.render("index"); // Nome do arquivo, o EJS já busca dentro da pasta views.
-  });
+app.get("/jogos", (req, res) => {
+  res.send("Aba jogos");
+});
+
+app.get("/filmes", (req, res) => {
+  res.send("Aba filmes");
+});
   
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
