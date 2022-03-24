b0081c5f2b066a81aa8f1a7e2967020f130d5c06
MicaMore <micael@email.com>
2022-03-23 23:42:06 -0300

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
+const pokedex = [
+  {
+    id: 1,
+    nome: "Bubassauro",
+    tipo: "Grass",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    descricao:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    altura: "0.7m",
+    peso: "6.9kg",
+    categoria: "Seed",
+    habilidade: "Overgrow",
+  },
+  {
+    id: 2,
+    nome: "Charmander",
+    tipo: "Fire",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+    descricao:
+      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+    altura: "0.6m",
+    peso: "8.5kg",
+    categoria: "Lizard",
+    habilidade: "Blaze",
+  },
+  {
+    id: 3,
+    nome: "Pidgey",
+    tipo: "Flying",
+    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png",
+    descricao:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+    altura: "0.3m",
+    peso: "1.8kg",
+    categoria: "Tiny Bird",
+    habilidade: "Keen Eye",
+  },
+];
+
+//Rotas
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render("index", {pokedex});
 });
 
-app.get("/jogos", (req, res) => {
-  res.send("Aba jogos");
-});
-
-app.get("/filmes", (req, res) => {
-  res.send("Aba filmes");
-});
-  
-app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
